b0f9f3cd52a5d38fe9b1086b3a3b98ea95cce5a4
Nathan Renfro <nathanrenfro@u.boisestate.edu>
2022-04-27 14:15:35 -0600

    Spee
    ch

---
+++ javascripts/speech.js
@@ -0,0 +1,92 @@
+const synth = window.speechSynthesis;
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelector = document.querySelector("#voice-select");
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');
+const body = document.querySelector('body');
+
+let voices = [];
+
+const getVoices = () => {
+  voices = synth.getVoices();
+
+  // Loop through voices and create an option for each one
+  voices.forEach(voice => {
+    // Create option element
+    const option = document.createElement('option');
+    // Fill option with voice and language
+    option.textContent = voice.name + '(' + voice.lang + ')';
+
+    // Set needed option attributes
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+    voiceSelector.appendChild(option);
+  });
+};
+getVoices();
+
+if (synth.onvoiceschanged !== undefined) {
+  synth.onvoiceschanged = getVoices;
+}
+// Speak
+const speak = () => {
+    // Check if speaking
+    if (synth.speaking) {
+      console.error('Already speaking...');
+      return;
+    }
+    if (textInput.value !== '') {
+      // Add background animation
+      body.style.background =
+        "#0acffe url(https://res.cloudinary.com/nonsoblip/image/upload/v1620236458/wave_n3rtre.gif)";
+      body.style.backgroundRepeat = 'repeat-x';
+      body.style.backgroundSize = '100% 100%';
+  
+      // Get speak text
+      const speakText = new SpeechSynthesisUtterance(textInput.value);
+  
+      // Speak end
+      speakText.onend = e => {
+        body.style.backgroundImage = `linear-gradient(to right, #0acffe 0%, #495aff 100%)`;
+      };
+  
+      // Speak error
+      speakText.onerror = e => {
+        console.error('Something went wrong');
+      };
+  
+      // Selected voice
+      const selectedVoice = voiceSelector.selectedOptions[0].getAttribute(
+        "data-name"
+      );
+  
+      // Loop through voices
+      voices.forEach(voice => {
+        if (voice.name === selectedVoice) {
+          speakText.voice = voice;
+        }
+      });
+  
+      // Set pitch and rate
+      speakText.rate = 1;
+      speakText.pitch = 1;
+      // Speak
+      synth.speak(speakText);
+    }
+  };
+
+  // EVENT LISTENERS
+
+// Text form submit
+textForm.addEventListener('submit', e => {
+    e.preventDefault();
+    speak();
+    textInput.blur();
+  });
+  
+  
+  // Voice select change
+  voiceSelector.addEventListener("change", (e) => speak());
